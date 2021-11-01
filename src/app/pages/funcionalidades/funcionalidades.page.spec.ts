1fba5dd543855ee6b5ff7d0314ec067f8b003ece
João Marcelo <souza.marcelo@escolar.ifrn.edu.br>
2021-11-01 13:59:46 -0300

    redirecionamento de login para funcionalidades

--- src/app/pages/funcionalidades/funcionalidades.page.spec.ts
+++ src/app/pages/funcionalidades/funcionalidades.page.spec.ts
@@ -1,24 +1,34 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 import { FuncionalidadesPage } from './funcionalidades.page';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 describe('FuncionalidadesPage', () => {
   let component: FuncionalidadesPage;
   let fixture: ComponentFixture<FuncionalidadesPage>;
+  let router : Router;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ FuncionalidadesPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        IonicModule.forRoot(),
+        AppRoutingModule
+      
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(FuncionalidadesPage);
+    router = TestBed.get(Router);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should go to login page on funcionalidades' , () =>{
+    spyOn(router ,'navigate');
+    component.funcionalidades();
+    expect(router.navigate).toHaveBeenCalledWith(['funcionalidades']);
+  })
 });
