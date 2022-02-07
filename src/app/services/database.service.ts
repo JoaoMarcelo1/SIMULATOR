f3162326b612941f0b01ff3ce77a5c5a80e88ad0
João Marcelo <souza.marcelo@escolar.ifrn.edu.br>
2022-02-07 20:45:11 -0200

    ajeitando bugs de declarar firebase

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -1,22 +1,23 @@
 import { Injectable } from '@angular/core';
 import { User } from '../interfaces/user';
-import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Injectable({
     providedIn: 'root'
   })
   export class databaseService {
-    constructor(private afa: AngularFireAuth) { }
+    afa: any;
+    constructor(private authfire: AngularFireAuth) { }
   
     login(user: User) {
-      return this.afa.auth.signInWithEmailAndPassword(user.email, user.password);
+      return this.afa.authfire.signInWithEmailAndPassword(user.email, user.password);
     }
   
     cadastro(user: User) {
-      return this.afa.auth.createUserWithEmailAndPassword(user.email, user.password);
+      return this.afa.authfire.createUserWithEmailAndPassword(user.email, user.password);
     }
     getAuth() {
-        return this.afa.auth;
+        return this.afa.authfire
       }
 
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,23 +5,24 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebase: {
+    apiKey: "AIzaSyDp2IuNXfSuf1ws-2i_VgiAROLq-FT_CUE",
+    authDomain: "simulator-19564.firebaseapp.com",
+    databaseURL: "https://simulator-19564-default-rtdb.firebaseio.com",
+    projectId: "simulator-19564",
+    storageBucket: "simulator-19564.appspot.com",
+    messagingSenderId: "778604917656",
+    appId: "1:778604917656:web:3afd9c866d10ca1e98f1ed",
+    measurementId: "G-NE22NN9S0H"
+  }
 };
 
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyDp2IuNXfSuf1ws-2i_VgiAROLq-FT_CUE",
-  authDomain: "simulator-19564.firebaseapp.com",
-  databaseURL: "https://simulator-19564-default-rtdb.firebaseio.com",
-  projectId: "simulator-19564",
-  storageBucket: "simulator-19564.appspot.com",
-  messagingSenderId: "778604917656",
-  appId: "1:778604917656:web:3afd9c866d10ca1e98f1ed",
-  measurementId: "G-NE22NN9S0H"
-};
+ 
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 
 
 
